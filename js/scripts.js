003485885eef07349d07ee466eeb82a6cd2f679e
Sunday Juma <samanthasundayjuma@gmail.com>
2018-11-29 14:12:03 +0300

    add adjusted js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 
 function myFunction() {
-var side a = document.getElementById('side a').value;
-var side b = document.getElementById('sideb').value;
-var base = document.getElementById('base').value;
+var side A = document.getElementById('side a').value;
+var side B = document.getElementById('sideb').value;
+var Base = document.getElementById('base').value;
 var result;
-if (base === side a && side a === side b && side b === base) {
+if (Base === side A && side A === side B && side B === Base) {
     result = "Equilateral Triangle";
-} else if (side a === side b && side b !== base && base !== side a){
-	result = "Isosceles Triangle"
-} else if (side a !== side b && side b !== base && base !==side a)(side a + side b < base && base + side a < side b && side b + base < side a) {
-	result = "Scalene Triangle"
-} else if ( side a + side b <= base && side b + base <= side a) { (side a + base <= side b) {
-	result = "Not a Triangle"
+} else if (side A === side B && side B !== Base && Base !== side A){
+	result = "Isosceles Triangle";
+} else if (side A !== side B && side B !== Base && Base !==side A)(side A + side B < Base && Base + side A < side B && side B + Base < side A) {
+	result = "Scalene Triangle";
+} else if ( side A + side B <= Base && side B + Base <= side A) { (side A + Base <= side B) {
+	result = "Not a Triangle";
 }else {
-  result ="Not a Triangle"
+  result ="Not a Triangle";
 }
 alert(result);
