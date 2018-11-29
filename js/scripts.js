145ae2ae4f6cd37505539797d45c4fc6e10b621b
Sunday Juma <samanthasundayjuma@gmail.com>
2018-11-29 11:30:31 +0300

    add initial javascript function

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+function myFunction() {
+var side a = document.getElementById('side a').value;
+var side b = document.getElementById('sideb').value;
+var base = document.getElementById('base').value;
+var result;
+if (base === side a && side a === side b) {
+  if (side b === base) {
+    result = "Equilateral Triangle";
+} else if (side a === side b && side b !== base) {(base !== side a){
+	result = "Isosceles Triangle"
+} else if (side a !== side b && side b !== base) {(base !==side a) {
+	result = "Scalene Triangle"
+} else if ( side a + side b <= base && side b + base <= side a) { (side a + base <= side b) {
+	result = "Not a Triangle"
+}else {
+  result ="Not a Triangle"
+}
+alert(result);
+}
