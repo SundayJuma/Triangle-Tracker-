efa8132dac1b4ab7be999222f608cf68d977fb78
Sunday Juma <samanthasundayjuma@gmail.com>
2018-11-30 13:44:44 +0300

    add modified js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-var a = parseInt (prompt("input a"));
-var b = parseInt (prompt("input b"));
-var c = parseInt (prompt("input c"));
+function triangleTracker(){
 
+var a = parseInt(document.getElementById("A")).value;
+var b = parseInt(document.getElementById("B")).value;
+var c = parseInt(document.getElementById("C")).value;
 
 if(isNaN (a)||isNaN (b) || isNaN (c)){
 alert("invalid input");
@@ -11,7 +12,7 @@ else if (a+b<=c || b+c<=a || c+a<=b) {
 alert("Not a Triangle");
 }
 
-if(a==b && b==c && c==a){
+else if(a==b && b==c && c==a){
 alert("Equilateral triangle");
 }
 else if (a==b || b==c || c==a){
@@ -19,4 +20,5 @@ alert("Isosceles Triangle");
 }
 else if (a!==b && b!==c && c!==a){
 alert("Scalene Triangle");
+}
 }
