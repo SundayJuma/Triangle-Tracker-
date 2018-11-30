ff457e7150523aa09cff38da4287376470a079d7
Sunday Juma <samanthasundayjuma@gmail.com>
2018-11-30 10:38:31 +0300

    add changes to js, css and html files

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,27 @@
 
-function myFunction() {
-var side A = document.getElementById('side a').value;
-var side B = document.getElementById('sideb').value;
-var Base = document.getElementById('base').value;
-var result;
-if (Base === side A && side A === side B && side B === Base) {
-    result = "Equilateral Triangle";
-} else if (side A === side B && side B !== Base && Base !== side A){
-	result = "Isosceles Triangle";
-} else if (side A !== side B && side B !== Base && Base !==side A)(side A + side B < Base && Base + side A < side B && side B + Base < side A) {
-	result = "Scalene Triangle";
-} else if ( side A + side B <= Base && side B + Base <= side A) { (side A + Base <= side B) {
-	result = "Not a Triangle";
-}else {
-  result ="Not a Triangle";
-}
-alert(result);
+
+  function event() {
+
+  var sideA = parseInt(document.getElementById('sideA').val());
+  var sideB = parseInt(document.getElementById('sideB').val());
+  var Base = parseInt(document.getElementById('Base').val());
+  var result;
+
+    if (Base === sideA && sideA === sideB && sideB === Base) {
+        result = "Equilateral Triangle";
+        alert(result);
+    } else if (sideA === sideB && sideB !== Base && Base !== sideA){
+    	result = "Isosceles Triangle";
+      alert(result);
+    } else if ((sideA !== sideB && sideB !== Base && Base !==sideA) && (sideA + sideB < Base && Base + sideA < sideB && sideB + Base < sideA)) {
+    	result = "Scalene Triangle";
+      alert(result);
+    } else if (sideA + sideB <= Base && sideB + Base <= sideA && sideA + Base <= sideB) {
+    	result = "Not a Triangle";
+      alert(result);
+    }else {
+      result ="Not a Triangle";
+      alert(result);
+    }
+
+  }
