ba4db27dd8b192cfc36745890cfa1863d4976ebe
Sunday Juma <samanthasundayjuma@gmail.com>
2018-11-30 13:35:02 +0300

    add modified js

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,22 @@
+var a = parseInt (prompt("input a"));
+var b = parseInt (prompt("input b"));
+var c = parseInt (prompt("input c"));
 
 
-  function event() {
+if(isNaN (a)||isNaN (b) || isNaN (c)){
+alert("invalid input");
+}
 
-  var sideA = parseInt(document.getElementById('sideA').val());
-  var sideB = parseInt(document.getElementById('sideB').val());
-  var Base = parseInt(document.getElementById('Base').val());
-  var result;
+else if (a+b<=c || b+c<=a || c+a<=b) {
+alert("Not a Triangle");
+}
 
-    if (Base === sideA && sideA === sideB && sideB === Base) {
-        result = "Equilateral Triangle";
-        alert(result);
-    } else if (sideA === sideB && sideB !== Base && Base !== sideA){
-    	result = "Isosceles Triangle";
-      alert(result);
-    } else if ((sideA !== sideB && sideB !== Base && Base !==sideA) && (sideA + sideB < Base && Base + sideA < sideB && sideB + Base < sideA)) {
-    	result = "Scalene Triangle";
-      alert(result);
-    } else if (sideA + sideB <= Base && sideB + Base <= sideA && sideA + Base <= sideB) {
-    	result = "Not a Triangle";
-      alert(result);
-    }else {
-      result ="Not a Triangle";
-      alert(result);
-    }
-
-  }
+if(a==b && b==c && c==a){
+alert("Equilateral triangle");
+}
+else if (a==b || b==c || c==a){
+alert("Isosceles Triangle");
+}
+else if (a!==b && b!==c && c!==a){
+alert("Scalene Triangle");
+}
